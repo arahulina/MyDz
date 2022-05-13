f29efbfe36b8a1d247c8db1b8272247605df29f4
Anna <>
2022-05-13 16:13:53 +0300

    Домашнє завдання 1

---
+++ JS_DZ_Modul_1_Week_2_1/DZ1/myScript.js
@@ -0,0 +1,19 @@
+/**
+ * рішення завдання №1
+ */
+let result = document.getElementById("result")
+
+document.getElementById("btnStart").onclick = function () {
+    let userNumber = document.getElementById("userNumber").value
+    if (userNumber >= 0 && userNumber < 12) {
+        result.innerText = "Дитина"
+    } else if (userNumber >= 12 && userNumber < 18) {
+        result.innerText = "Підліток"
+    } else if (userNumber >= 18 && userNumber < 60) {
+        result.innerText = "Дорослий"
+    }
+    else {
+        result.innerText = "Пенсіонер"
+    }
+
+}
