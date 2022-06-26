aec7a2085c29a8a7293328afc2b78a6dd89c0cc2
Anna <>
2022-06-26 13:19:47 +0300

    Diagram

--- Express/controllers/productsControllers.js
+++ Express/controllers/productsControllers.js
@@ -1,15 +1,10 @@
 
 let products = [
-    {id: 1, name: 'Альфа-Банк', profit: '3,35'},
-    {id: 2, name: 'ОТП Банк', profit: '2,75'},
-    {id: 3, name: 'Приватбанк', profit: '35,05'},
-    {id: 4, name: 'ПУМБ', profit: '4,19'},
-    {id: 5, name: 'Райффайзен Банк', profit: '4,86'},
-    {id: 6, name: 'Сбербанк/МР Банк', profit: '3,9'},
-    {id: 7, name: 'Сітібанк', profit: '1,77'},
-    {id: 8, name: 'Укрексімбанк', profit: '2,68'},
-    {id: 9, name: 'Укргазбанк', profit: '3,82'},
-    {id: 10, name: 'Універсал Банк', profit: '3,07'},
+    {id: 1, name: 'Аватар', profit: '3286'},
+    {id: 2, name: 'Унесёенные ветром', profit: '3739'},
+    {id: 3, name: 'Титаник', profit: '3108'},
+    {id: 4, name: 'Звёздные войны. Эпизод IV: Новая надежда', profit: '3071'},
+    {id: 5, name: 'Мстители: Финал', profit: '2823'},
 ]
 
 exports.index = function (request, response) {
